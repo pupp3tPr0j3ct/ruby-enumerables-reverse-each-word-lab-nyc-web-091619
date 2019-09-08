65ba53220f9b3e68f23aa605ad14b353e7203e1e
Jason Santana <jsantana006@gmail.com>
2019-09-08 21:50:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,12 @@
 def reverse_each_word(string)
-  new_string = string.split #=> "Hello there, and how are you?"  is now ["Hello", "there,", "and", "how", "are", "you?"]
+  new_string = string.split #=> 
+  # "Hello there, and how are you?"  is now an array ["Hello", "there,", "and", "how", "are", "you?"]
   new_array = []
   m = ''
   
   new_string.collect do
     #new_string[m].reverse
-    new_array.push(new_string[m].reverse)
+    new_array.push(new_string.reverse)
     result = new_array.join(' ')
   end
   
