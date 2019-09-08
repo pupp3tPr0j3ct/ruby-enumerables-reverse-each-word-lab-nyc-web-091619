b2d9d9f7e1b65beeca16a3c6395b694242e64e9d
Jason Santana <jsantana006@gmail.com>
2019-09-08 21:40:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,14 @@
 def reverse_each_word(string)
   new_string = string.split
-  
+  new_array = []
   #collect each space separated string in an array by use of a loop, every space denotes a separation point
   
   new_string.collect do |m|
     new_string[0] = m
-    new_string[m].reverse
+    #new_string[m].reverse
+    new_array.push(new_string[m].reverse)
+    
   end
-  #reverse each indexed string
+  #join each reversed string
     
 end
