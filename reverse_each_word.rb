62da35ddf719c07b9963745cab9e6904dbde626d
Jason Santana <jsantana006@gmail.com>
2019-09-08 21:32:35 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,7 @@
+def reverse_each_word(string)
+  
+  #collect each space separated string in an array by use of a loop, every space denotes a separation point
+
+  #reverse each indexed string
+    
+end
