4f85abea8279472a426f73e6c4576e54c718b01a
Jason Santana <jsantana006@gmail.com>
2019-09-08 22:08:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,10 +3,10 @@ def reverse_each_word(string)
   # "Hello there, and how are you?"  is now an array ["Hello", "there,", "and", "how", "are", "you?"]
   new_array = []
   
-  new_string.collect do |m, n|
+  new_string.collect do |m|
     new_string[0] = m
-    n = m.reverse
-    return n
+    new_string.reverse
+    
   end
 
 end
